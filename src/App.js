eb3e9970b6526e167fcbe417107f71ab1bb31ab8
dylan belouis <dylan.belouis@gmail.com>
2019-10-17 16:17:02 +0200

    page loser fonctionnel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import React,{ Component } from 'react';
+=======
+<<<<<<< HEAD
+import React,{Component} from 'react';
+>>>>>>> page loser fonctionnel
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import BattlePage from './components/BattlePage/BattlePage'
@@ -7,6 +12,7 @@ import Loading from './components/Loading/Loading'
 
 
 class App extends Component {
+<<<<<<< HEAD
   render(){
   return(
     <Router>
@@ -22,6 +28,38 @@ class App extends Component {
     </Router>
   )
   }
+=======
+    render(){
+      return(
+        <Router>
+          <div>
+              <Switch>
+                {/* <Route exact path="/" component={DisplayCardSelect} />*/}
+                <Route path="/battle" component={BattlePage} />
+                <Route path="/load" component={Loading} />
+                {/*<Route path="/win" component={Winner} /> */}
+                <Route path="/loose" component={LoserPage} />
+              </Switch>
+          </div>
+        </Router>
+      )
+    }
+=======
+import React from 'react';
+import LoserPage from './components/Loser/LoserPage.js';
+
+/*import logo from './logo.svg';
+import './App.css';*/
+
+
+function App() {
+  return (
+    <div className="App">
+     <LoserPage />
+    </div>
+  );
+>>>>>>> page loser fonctionnel
+>>>>>>> page loser fonctionnel
 }
 
 export default App;
--- src/components/Loser/LoserPage.js
+++ src/components/Loser/LoserPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import cross from './croixtest.png'; 
 import './LoserPage.css';
+<<<<<<< HEAD
 import Row from './return.png';
 import PokeFinal from './pokefinal.png';
 
@@ -11,10 +12,19 @@ function LoserPage(){
             <div className="loser-poke">
                 <img src={PokeFinal} alt="pokefinal" />
             </div>
+=======
+import returne from './return.png';
+
+
+function LoserPage () {
+    return (
+        <div className="container">
+>>>>>>> page loser fonctionnel
             <div className="element-animation2">
                 <img className="cross" src={cross} alt="lose"/>
             </div>
             <div className="element-animation">
+<<<<<<< HEAD
                 <h2 className="loser--title">You loose</h2>
             </div>
             <div className="bounce-in-bottom">
@@ -25,3 +35,15 @@ function LoserPage(){
 }
 
 export default LoserPage;
+=======
+            <h2>You lose</h2>
+            </div>
+            <div className="return">
+                <img className="retour" src={returne} alt="return to game" /> 
+            </div>
+        </div>
+    );
+}
+
+export default LoserPage;
+>>>>>>> page loser fonctionnel
