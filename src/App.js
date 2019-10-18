19eda848769f0f0650281f1414ee916dcbe3e8cc
jn-html <marmainjean@gmail.com>
2019-10-18 11:19:09 +0200

    Add animation on winner page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React,{Component} from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
@@ -22,6 +23,19 @@ class App extends Component {
         </Router>
       )
     }
+=======
+import React from 'react';
+/*import logo from './logo.svg';*/
+import './App.css';
+import Winner from './components/WinnerPage/Winner';
+
+function App() {
+  return (
+    <div className="App">
+      < Winner />
+    </div>
+  );
+>>>>>>> Add animation on winner page
 }
 
 export default App;
