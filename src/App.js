f2da2e1fb2f309070d115bc3ecd29b40a92f3355
Rémy <Remypottierpro@gmail.com>
2019-10-22 17:38:18 +0200

    Set Router with components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React,{Component} from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
+import ShowChooseCard from './components/ShowChooseCard'
 
 class App extends Component {
     render(){
       return(
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={ChoicePage} />
-            <Route path="/load" component={Loader} />
-            <Route path="/app" component={CombatPage} />
-            <Route path="/win" component={Winner} />
-            <Route path="/loose" component={Loser} />
+            <Route exact path="/" component={ShowChooseCard} />
+            <Route path="/app" component={ShowChooseCard} />
+            {/* <Route path="/load" component={Loader} /> */}
+            {/* <Route path="/win" component={Winner} />
+            <Route path="/loose" component={Loser} /> */}
           </Switch>
         </BrowserRouter>
       )
---
+++ src/components/ChooseCard.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import './ChooseCard.css';
+
+class ChooseCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            image : props.image,
+            name: props.name,
+            type: props.type,
+            description: props.desc,
+            atks:props.atks,
+            pv:props.pv
+        }
+    }
+    
+    render(){
+        return(
+            <section className="choose-card element-animation">
+                <div className="choose-card--content">
+                    <div className="choose-card--info">
+                        <span className="choose-card--info-name">{this.state.name}</span>
+                        <div>
+                            <p className="choose-card--info-pv">{this.state.pv} PV</p>
+                            <img className="choose-card--info-type" alt="Type" src={this.state.type}/>
+                        </div>
+                    </div>  
+                    <img className="choose-card--avatar" alt="Pokémon" src={this.state.image}/>
+                    <div className="choose-card--atks">
+                        <div className="choose-card--atks-panel">
+                            <span className="choose-card--atks-panel-name">
+                                <img className="choose-card--info-type" alt="Type" src={this.state.type}/>
+                                Morsure
+                            </span>
+                            <span className="choose-card--atks--panel-damg">20</span>
+                        </div>
+                        <div className="choose-card--atks-panel">
+                            <span className="choose-card--atks-panel-name">
+                                <img className="choose-card--info-type" alt="Type" src={this.state.type}/>
+                                Morsure
+                            </span>
+                            <span className="choose-card--atks--panel-damg">20</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            )
+    }
+}
+
+export default ChooseCard
