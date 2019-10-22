e4215826cffd63479a7bdcf6f6459420fff24f36
Rémy <Remypottierpro@gmail.com>
2019-10-22 17:12:32 +0200

    Create Routing of project ( see in App.js )

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React,{Component} from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+    render(){
+      return(
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ChoicePage} />
+            <Route path="/load" component={Loader} />
+            <Route path="/app" component={CombatPage} />
+            <Route path="/win" component={Winner} />
+            <Route path="/loose" component={Loser} />
+          </Switch>
+        </BrowserRouter>
+      )
+    }
 }
 
 export default App;
