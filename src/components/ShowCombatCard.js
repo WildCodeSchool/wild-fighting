0e201a9bb8adb45c15cae0d7fd631809dfdbd43d
Rémy <Remypottierpro@gmail.com>
2019-10-23 13:57:47 +0200

    Change detail for intégration to React Router

--- src/components/ShowCombatCard.js
+++ src/components/ShowCombatCard.js
@@ -4,7 +4,7 @@ import './CombatCard.css'
 
 function ShowCombatCard() {
   return (
-    <div className="App">
+    <div className="ShowCombatCard">
       <CombatCard image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/126.png" name="Pikachu" type="https://www.pokepedia.fr/images/e/e5/Miniature_Type_Feu_JCC.png" pv='70' />
       <CombatCard image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/125.png" name="Pikachu" type="https://www.pokepedia.fr/images/e/e5/Miniature_Type_Feu_JCC.png" pv='70' />
     </div>
