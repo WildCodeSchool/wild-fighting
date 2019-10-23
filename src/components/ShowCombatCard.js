395f481b2ed2d823d1980c507291eb27f1ee500f
Rémy <Remypottierpro@gmail.com>
2019-10-23 13:34:49 +0200

    Set component for React Router

---
+++ src/components/ShowCombatCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CombatCard from './CombatCard'
+import './CombatCard.css'
+
+function ShowCombatCard() {
+  return (
+    <div className="App">
+      <CombatCard image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/126.png" name="Pikachu" type="https://www.pokepedia.fr/images/e/e5/Miniature_Type_Feu_JCC.png" pv='70' />
+      <CombatCard image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/125.png" name="Pikachu" type="https://www.pokepedia.fr/images/e/e5/Miniature_Type_Feu_JCC.png" pv='70' />
+    </div>
+  );
+}
+
+export default ShowCombatCard;
