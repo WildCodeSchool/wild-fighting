d89787c7ab766e5cd30526e44ced240be331e912
jn-html <marmainjean@gmail.com>
2019-10-24 11:15:04 +0200

    Add modif

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-/*import logo from './logo.svg';*/
 import './App.css';
 import Winner from './components/WinnerPage/Winner';
 
 function App() {
   return (
-    <div className="App">
-      < Winner />
-    </div>
+    
   );
 }
 
