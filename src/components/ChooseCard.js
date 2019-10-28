d479ce6615be11b32385c6c73643c7e30029091e
Rémy <Remypottierpro@gmail.com>
2019-10-28 16:17:42 +0100

    Start random selection for pokemon2

--- src/components/ChooseCard.js
+++ src/components/ChooseCard.js
@@ -5,6 +5,7 @@ class ChooseCard extends Component {
     constructor(props) {
         super(props)
         this.state={
+            key:props.key,
             image : props.image,
             name: props.name,
             type: props.type,
@@ -12,6 +13,50 @@ class ChooseCard extends Component {
             atks:props.atks,
             pv:props.pv
         }
+        this.type = this.type.bind(this)
+    }
+    type(type){
+        let typeResult = []
+        type.map(type=>{
+            if(type==="fire"){
+                return typeResult.push('fire')
+            }
+            else if(type==="water"){
+                return typeResult.push('water')
+            }
+            else if(type==="plant"){
+                return typeResult.push('plant')
+            }
+            else if(type==="electrik"){
+                return typeResult.push('electrik')
+            }
+            else if(type==="psychic"){
+                return typeResult.push('psychic')
+            }
+            else if(type==="fighting"){
+                return typeResult.push('fighting')
+            }
+            else if(type==="normal"){
+                return typeResult.push('normal')
+            }
+            else if(type==="ghost"){
+                return typeResult.push('ghost')
+            }
+            else if(type==="dragon"){
+                return typeResult.push('dragon')
+            }
+            else{
+                return false
+            }
+    })
+        console.log(typeResult)
+        window.addEventListener("DOMContentLoaded", (e) => {
+            let element = ""
+            for(let i = 0;i<typeResult.length;i+=1){
+                element +=`<img src="${typeResult[i]}.png" class="type--image" />`
+            }
+            document.querySelector('.types'+this.props.id).innerHTML += element
+        });
     }
     render(){
         return(
@@ -21,7 +66,8 @@ class ChooseCard extends Component {
                         <span className="choose-card--info-name">{this.state.name}</span>
                         <div>
                             <p className="choose-card--info-pv">{this.state.pv} PV</p>
-                            <img className="choose-card--info-type" alt="Type" src={this.state.type}/>
+                            {console.log("type : "+this.state.type)}
+                            <div className={`types${this.props.id}`}>{this.type(this.state.type)}</div>
                         </div>
                     </div>  
                     <img className="choose-card--avatar" alt="Pokémon" src={this.state.image}/>
