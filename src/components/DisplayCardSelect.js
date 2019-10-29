e9c3f7fb1c4f4037a1059fd9bff8420b7b128d2e
Rémy <Remypottierpro@gmail.com>
2019-10-29 11:15:35 +0100

    solve pull request

--- src/components/DisplayCardSelect.js
+++ src/components/DisplayCardSelect.js
@@ -34,9 +34,9 @@ class DisplayCardSelect extends Component {
           this.state.pokemon2 ?
           (
           <div className="App">
-            <ChooseCard id="0" image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} hp='70' />
+            <CardSelect id="0" image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} hp='70' />
             <div className="App--VS"><button className="App--VS-button">V S</button></div>
-            <ChooseCard id="1" image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} hp='70' />
+            <CardSelect id="1" image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} hp='70' />
           </div>
           ):
           <p>LOADING</p>
