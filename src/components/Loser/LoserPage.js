ad86af2be5f73e8602cf304dbdd54268736fe71e
dylan belouis <dylan.belouis@gmail.com>
2019-10-29 17:26:46 +0100

    ajout responsive design

--- src/components/Loser/LoserPage.js
+++ src/components/Loser/LoserPage.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import cross from './croixtest.png'; 
 import './LoserPage.css';
-import Row from './return.png'
+import Row from './return.png';
+import PokeFinal from './pokefinal.png'
 
 
 function LoserPage(){
         return(
             <div className="container">
-                <div className="loserpoke">
-                    <div className="element-animation2">
-                        <img className="cross" src={cross} alt="lose"/>
-                    </div>
+                <div className="loser-poke">
+                    <img src={PokeFinal} />    
+                    
+                </div>
+                <div className="element-animation2">
+                    <img className="cross" src={cross} alt="lose"/>
+                </div>
                 <div className="element-animation">
                     <h2>You loose</h2>
                 </div>
                 <div className="bounce-in-bottom">
                     <img className="row" src={Row} alt="lose"/>
-                </div>
                 </div>
             </div>
         )
