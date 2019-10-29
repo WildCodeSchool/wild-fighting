245fafabb365fd4c4fee626d4856045a3b9155b9
dylan belouis <dylan.belouis@gmail.com>
2019-10-29 17:42:57 +0100

    ajout font familly pokemon

--- src/components/Loser/LoserPage.js
+++ src/components/Loser/LoserPage.js
@@ -3,15 +3,19 @@ import cross from './croixtest.png';
 import './LoserPage.css';
 import Row from './return.png';
 import PokeFinal from './pokefinal.png';
+<<<<<<< HEAD
 
 
 
 
+=======
+>>>>>>> ajout font familly pokemon
 
 
 function LoserPage(){
         return(
             <div className="container">
+<<<<<<< HEAD
 <<<<<<< HEAD
             <div className="element-animation2">
                 <img className="cross" src={cross} alt="lose"/>
@@ -34,8 +38,17 @@ export default LoserPage;
                     <div className="element-animation2">
                         <img className="cross" src={cross} alt="lose"/>
                     </div>
+=======
+                <div className="loser-poke">
+                    <img src={PokeFinal} alt="pokefinal" />    
+                    
+                </div>
+                <div className="element-animation2">
+                    <img className="cross" src={cross} alt="lose"/>
+                </div>
+>>>>>>> ajout font familly pokemon
                 <div className="element-animation">
-                    <h2>You loose</h2>
+                    <h2 style={{fontFamily:"PokemonSolid"}}>You loose</h2>
                 </div>
                 <div className="bounce-in-bottom">
                     <img className="row" src={Row} alt="lose"/>
