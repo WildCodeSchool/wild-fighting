dc2c9962fbdfbcee25ee179ef856f787a08b1724
Rémy <Remypottierpro@gmail.com>
2019-10-29 10:55:52 +0100

    Finish Random card

--- src/components/ShowChooseCard.js
+++ src/components/ShowChooseCard.js
@@ -7,6 +7,7 @@ class ShowChooseCard extends Component {
   constructor(props){
     super(props)
     this.state = {
+      pokemon1 : null,
       pokemon2 : null
     }
     this.axiosRequest= this.axiosRequest.bind(this)
@@ -18,6 +19,7 @@ class ShowChooseCard extends Component {
     const min = Math.ceil(1);
     const max = Math.floor(101);
     const number =  Math.floor(Math.random() * (max - min)) + min;
+    console.log(number)
     axios.get("https://pokeapi.co/api/v2/pokemon/"+number)
       .then( response => response.data )
       .then( pokemon =>{
@@ -32,9 +34,9 @@ class ShowChooseCard extends Component {
           this.state.pokemon2 ?
           (
           <div className="App">
-            {/* <ChooseCard image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png" name="Pikachu" type="https://www.pokepedia.fr/images/e/e5/Miniature_Type_Feu_JCC.png" pv='70'/> */}
+            <ChooseCard id="0" image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} hp='70' />
             <div className="App--VS"><button className="App--VS-button">V S</button></div>
-            <ChooseCard image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} id="1" pv='70' />
+            <ChooseCard id="1" image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} hp='70' />
           </div>
           ):
           <p>LOADING</p>
