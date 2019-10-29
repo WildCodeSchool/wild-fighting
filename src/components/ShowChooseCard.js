30a2850b6851075231b6cdb8818817ae427d92b9
Rémy <Remypottierpro@gmail.com>
2019-10-29 09:18:06 +0100

    WIP random card

--- src/components/ShowChooseCard.js
+++ src/components/ShowChooseCard.js
@@ -34,7 +34,7 @@ class ShowChooseCard extends Component {
           <div className="App">
             {/* <ChooseCard image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png" name="Pikachu" type="https://www.pokepedia.fr/images/e/e5/Miniature_Type_Feu_JCC.png" pv='70'/> */}
             <div className="App--VS"><button className="App--VS-button">V S</button></div>
-            <ChooseCard image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} pv='70' />
+            <ChooseCard image={this.state.pokemon2.sprites.front_default} name={this.state.pokemon2.name} type={this.state.pokemon2.types.lenght>=2 ? [this.state.pokemon2.types[0].type.name,this.state.pokemon2.types[1].type.name]:[this.state.pokemon2.types[0].type.name]} id="1" pv='70' />
           </div>
           ):
           <p>LOADING</p>
