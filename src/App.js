cb4ea953b60fbc61fc10201c9ef890c13b6de631
rodolphe37 <rodolphe.a@gmail.com>
2019-10-29 14:26:25 +0100

    Revision du css et responsive mediaqueries

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+/*import './App.css';*/
+import Loading from "./components/Loading/Loading";
+
+function App() {
+  return (
+    <div className="App">
+      <Loading />
+    </div>
+  );
+}
+
+export default App;
