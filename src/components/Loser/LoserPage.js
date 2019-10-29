f671a727cb46b6fc362595df502f3d558cac9aa2
dylan belouis <dylan.belouis@gmail.com>
2019-10-29 09:41:15 +0100

    correction de la pull request

--- src/components/Loser/LoserPage.js
+++ src/components/Loser/LoserPage.js
@@ -8,18 +8,17 @@ function LoserPage(){
         return(
             <div className="container">
                 <div className="loserpoke">
-            <div className="element-animation2">
-                <img className="cross" src={cross} alt="lose"/>
+                    <div className="element-animation2">
+                        <img className="cross" src={cross} alt="lose"/>
+                    </div>
+                <div className="element-animation">
+                    <h2>You loose</h2>
+                </div>
+                <div className="bounce-in-bottom">
+                    <img className="row" src={Row} alt="lose"/>
+                </div>
+                </div>
             </div>
-            <div className="element-animation">
-            <h2>You loose</h2>
-            </div>
-            <div className="bounce-in-bottom">
-            <img className="row" src={Row} alt="lose"/>
-                
-            </div>
-        </div>
-        </div>
         )
     }
 
