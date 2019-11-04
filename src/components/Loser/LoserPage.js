06bd8104bdd097207114f3e1a21f740b72bc7153
Rémy <Remypottierpro@gmail.com>
2019-11-04 15:38:37 +0100

    Fix For PR

--- src/components/Loser/LoserPage.js
+++ src/components/Loser/LoserPage.js
@@ -9,14 +9,13 @@ function LoserPage(){
         return(
             <div className="container">
                 <div className="loser-poke">
-                    <img src={PokeFinal} alt="pokefinal" />    
-                    
+                    <img src={PokeFinal} alt="pokefinal" />
                 </div>
                 <div className="element-animation2">
                     <img className="cross" src={cross} alt="lose"/>
                 </div>
                 <div className="element-animation">
-                    <h2 style={{fontFamily:"PokemonSolid"}}>You loose</h2>
+                    <h2 className="loser--title">You loose</h2>
                 </div>
                 <div className="bounce-in-bottom">
                     <img className="row" src={Row} alt="lose"/>
