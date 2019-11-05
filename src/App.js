8cb148e352ce8701bc6cabb7f01fdc9777317ac0
jn-html <marmainjean@gmail.com>
2019-11-05 10:43:38 +0100

    move fonts folder

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ class App extends Component {
         <Router>
           <div>
               <Switch>
-                <Route exact path="/" component={DisplayCardSelect} />
+                {/* <Route exact path="/" component={DisplayCardSelect} />
                 <Route path="/app" component={DisplayCardFight} />
                 <Route path="/load" component={Loading} />
-                <Route path="/win" component={Winner} />
+                <Route path="/win" component={Winner} /> */}
                 <Route path="/loose" component={LoserPage} />
               </Switch>
           </div>
