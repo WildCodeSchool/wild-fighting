dd7586100dfaadbcde126f9cf322c4e34186400b
Rémy <Remypottierpro@gmail.com>
2019-11-05 09:31:40 +0100

    add dependenties of modal

--- src/App.js
+++ src/App.js
@@ -12,24 +12,22 @@ import Onglet from './components/Onglet/Onglet';
 
 
 class App extends Component {
-  render(){
-  return (
-    <>
-    <Onglet />
-    <Router>
-      <div>
-        <Switch>
-          {/* <Route exact path="/" component={DisplayCardSelect} />*/}
-          <Route path="/battle" component={BattlePage} />
-          <Route path="/load" component={Loading} />
-          <Route path="/win" component={WinnerPage} />
-          <Route path="/pokedex" component={ModalPage} />
-          <Route path="/loose" component={LoserPage} />
-        </Switch>
-      </div>
-    </Router>
-    </>
-  )
+  render() {
+    return (
+      <Router>
+        <div>
+          <Onglet />
+          <Switch>
+            {/* <Route exact path="/" component={DisplayCardSelect} /> */}
+            <Route path="/battle" component={BattlePage} />
+            <Route path="/load" component={Loading} />
+            <Route path="/win" component={WinnerPage} />
+            <Route path="/pokedex" component={ModalPage} />
+            <Route path="/loose" component={LoserPage} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 export default App;
