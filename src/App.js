79c180c30c87730d4aa8a92f6b6848064519c44a
jn-html <marmainjean@gmail.com>
2019-11-05 10:17:59 +0100

    use sanitize instead of inline reset

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
 
+
 class App extends Component {
     render(){
       return(
