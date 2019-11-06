025e5530f5d6add2a34c59b14de677c964b82ae8
dylan belouis <dylan.belouis@gmail.com>
2019-11-06 14:11:39 +0100

    ajout ESLint et fix indentation LoserPage

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,7 @@ module.exports ={
     extends: 'airbnb',
     rules: {
         'react/jsx-filename-extension':[1, {extensions:['.js']}],
+        "react/prop-types": 0,
+        "max-len": [2, {"code": 256, "tabWidth": 2, "ignoreUrls": true}]
     }
 };
