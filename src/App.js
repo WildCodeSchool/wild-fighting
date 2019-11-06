33e1d7ff907ababc1e0b5d5f4451cce27d48b9d4
rodolphe37 <rodolphe.a@gmail.com>
2019-11-06 10:11:43 +0100

    installation of eslint (airbnb)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React,{ Component } from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import BattlePage from './components/BattlePage/BattlePage'
@@ -7,21 +7,21 @@ import Loading from './components/Loading/Loading'
 
 
 class App extends Component {
-    render(){
-      return(
-        <Router>
-          <div>
-              <Switch>
-                {/* <Route exact path="/" component={DisplayCardSelect} />*/}
-                <Route path="/battle" component={BattlePage} />
-                <Route path="/load" component={Loading} />
-                {/*<Route path="/win" component={Winner} /> */}
-                <Route path="/loose" component={LoserPage} />
-              </Switch>
-          </div>
-        </Router>
-      )
-    }
+  render(){
+  return(
+    <Router>
+      <div>
+        <Switch>
+          {/* <Route exact path="/" component={DisplayCardSelect} />*/}
+          <Route path="/battle" component={BattlePage} />
+          <Route path="/load" component={Loading} />
+          {/*<Route path="/win" component={Winner} /> */}
+          <Route path="/loose" component={LoserPage} />
+        </Switch>
+      </div>
+    </Router>
+  )
+  }
 }
 
 export default App;
