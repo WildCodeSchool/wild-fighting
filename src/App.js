0d907b47eaed3d4d16076d2e36fe601f2ed3f229
dylan belouis <dylan.belouis@gmail.com>
2019-11-07 11:24:29 +0100

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React,{ Component } from 'react';
-=======
-<<<<<<< HEAD
-=======
->>>>>>> ajout pour rebase
 import React,{Component} from 'react';
->>>>>>> page loser fonctionnel
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import BattlePage from './components/BattlePage/BattlePage'
 import LoserPage from './components/Loser/LoserPage';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Loading from './components/Loading/Loading'
 
 
 class App extends Component {
-<<<<<<< HEAD
-  render(){
-  return(
-    <Router>
-      <div>
-        <Switch>
-          {/* <Route exact path="/" component={DisplayCardSelect} />*/}
-          <Route path="/battle" component={BattlePage} />
-          <Route path="/load" component={Loading} />
-          {/*<Route path="/win" component={Winner} /> */}
-          <Route path="/loose" component={LoserPage} />
-        </Switch>
-      </div>
-    </Router>
-  )
-  }
-=======
     render(){
       return(
         <Router>
@@ -49,45 +22,6 @@ class App extends Component {
         </Router>
       )
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import React from 'react';
-import LoserPage from './components/Loser/LoserPage.js';
-
-=======
-=======
-import TestApi from './components/TestApi';
->>>>>>> resolution de conflit
-/*import logo from './logo.svg';
-import './App.css';*/
-=======
-
-
->>>>>>> correction de la pull request
-
-
-function App() {
-  return (
-    <div className="App">
-<<<<<<< HEAD
-     <LoserPage />
-<<<<<<< HEAD
-    </div>
-  );
->>>>>>> page loser fonctionnel
->>>>>>> page loser fonctionnel
-=======
->>>>>>> ajout pour rebase
-=======
-     <TestApi />
-=======
-      <LoserPage />
->>>>>>> resolve conflict
-    </div>
-  );
->>>>>>> ajout API dans la console
->>>>>>> resolution de conflit
 }
 
 export default App;
--- src/components/Loser/LoserPage.js
+++ src/components/Loser/LoserPage.js
@@ -3,59 +3,30 @@ import cross from './croixtest.png';
 import './LoserPage.css';
 import Row from './return.png';
 import PokeFinal from './pokefinal.png';
-<<<<<<< HEAD
 
 
 
 
-=======
->>>>>>> ajout font familly pokemon
 
 
 function LoserPage(){
         return(
             <div className="container">
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <div className="element-animation2">
-                <img className="cross" src={cross} alt="lose"/>
+                <div className="element-animation2">
+                    <img className="cross" src={cross} alt="lose"/>
+                </div>
+                <div className="element-animation">
+                    <h2 className="loser--title">You loose</h2>
+                </div>
+                <a href>
+                <div className="bounce-in-bottom">
+                    <img className="row" src={Row} alt="lose"/>
+                </div>
+                </a>
             </div>
-            <div className="element-animation">
-
-                <h2 className="loser--title">You loose</h2>
-            </div>
-            <div className="bounce-in-bottom">
-                <img className="row" src={Row} alt="lose"/>
-            </div>
-        </div>
     )
 }
 
 export default LoserPage;
 
-=======
-                <div className="loserpoke">
-                    <div className="element-animation2">
-                        <img className="cross" src={cross} alt="lose"/>
-                    </div>
-=======
-                <div className="loser-poke">
-                    <img src={PokeFinal} alt="pokefinal" />    
-                    
-                </div>
-                <div className="element-animation2">
-                    <img className="cross" src={cross} alt="lose"/>
-                </div>
->>>>>>> ajout font familly pokemon
-                <div className="element-animation">
-                    <h2 style={{fontFamily:"PokemonSolid"}}>You loose</h2>
-                </div>
-                <div className="bounce-in-bottom">
-                    <img className="row" src={Row} alt="lose"/>
-                </div>
-                </div>
-            </div>
-        )
-    }
->>>>>>> correction de la pull request
 
