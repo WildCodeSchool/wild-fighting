95ac40cbb883601e02c0af1e78d565abbd7f0cfb
jn-html <marmainjean@gmail.com>
2019-11-13 16:00:51 +0100

    Add Winner Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import BattlePage from './components/BattlePage/BattlePage'
 import LoserPage from './components/Loser/LoserPage';
-import Loading from './components/Loading/Loading'
+import Loading from './components/Loading/Loading';
+import WinnerPage from './components/WinnerPage/WinnerPage';
 
 
 class App extends Component {
@@ -12,10 +13,11 @@ class App extends Component {
     <Router>
       <div>
         <Switch>
+          <Route exact path ="/" component={Loading} />
           {/* <Route exact path="/" component={DisplayCardSelect} />*/}
           <Route path="/battle" component={BattlePage} />
           <Route path="/load" component={Loading} />
-          {/*<Route path="/win" component={Winner} /> */}
+          <Route path="/win" component={WinnerPage} />
           <Route path="/loose" component={LoserPage} />
         </Switch>
       </div>
